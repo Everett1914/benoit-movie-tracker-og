b0f54e1afb5f1ac0c61bd813cf2c30d82bd92198
Everett1914 <qwest1914@gmail.com>
2021-09-13 22:38:25 -0400

    Add everything

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FulltableComponent } from './fulltable/fulltable.component';
+import { MoviedashboardComponent } from './moviedashboard/moviedashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'fulltable', component: FulltableComponent },
+	{ path: 'moviedashboard', component: MoviedashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviedashboardComponent } from './moviedashboard/moviedashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FulltableComponent } from './fulltable/fulltable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviedashboardComponent,
+    FulltableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	MatTableModule,
+	MatButtonToggleModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
